f3650bcd2b3c6260cd4862a1c354aa20e35371cb
Marcestordeur <marcstord@gmail.com>
2024-04-27 18:42:59 -0300

    call the models

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,1 +1,62 @@
 require('dotenv').config();
+const { User } = require('../Models/usersModel');
+const { Event } = require('../Models/eventsModel');
+const { Restaurant } = require('../Models/restaurantModel')
+
+const createRestaurant = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  };
+};
+const createEvent = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  };
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const deleteRestaurant = async (req,res) => {
+  try {
+    
+  } catch (error) {
+    
+  };
+};
+const deleteEvent = async (req, res) => {
+try {
+  
+} catch (error) {
+  
+};
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const getAllUsers = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+};
+const getAllRestaurants = async (req,res) => {
+  try {
+    
+  } catch (error) {
+    
+  };
+};
+const getAllEvents = async (req,res) => {
+  try {
+    
+  } catch (error) {
+    
+  };
+};
