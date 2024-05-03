98249f545a17877e22de41eb55e85b1a36207f1f
Marcestordeur <marcstord@gmail.com>
2024-05-03 16:07:57 -0300

    add functions createRes & createEvent

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,20 +1,45 @@
 require('dotenv').config();
 const { User } = require('../Models/usersModel');
 const { Event } = require('../Models/eventsModel');
-const { Restaurant } = require('../Models/restaurantModel')
+const { Restaurant, Restaurant } = require('../Models/restaurantModel');
+const e = require('express');
 
 const createRestaurant = async (req, res) => {
   try {
-    
+    const {Title, Information1, Information2, Menu} = req.body
+    const restaurant = await Restaurant.findOne({Title});
+    if(restaurant){
+      return res.status(400).json({message: 'the restaurant already exist'});
+    }else if(!restaurant){
+      const newRestaurant = new Restaurant ({
+        Title,
+        Information1,
+        Information2,
+        Menu
+      });
+      await newRestaurant.save();
+      res.status(200).json({message: 'the restaurant was created successfully'});
+    };
   } catch (error) {
-    
+    res.status(500).json({message: 'we cannot create the restaurant, sorry for the problems', error: error.message});
   };
 };
 const createEvent = async (req, res) => {
   try {
-    
+    const {} = req.body;
+    const event =  await Event.findOne({Title});
+    if(event){
+      return res.status(400).json({message:''});
+    }else if(!event){
+      const newEvent = new Event({
+        Title,
+        Information1
+      });
+      await newEvent.save();
+      res.status(200).json({message: 'the Event was created successfully'}); 
+    };
   } catch (error) {
-    
+    res.status(500).json({message: 'we cannot create the Event, sorry for the problems', error: error.message});
   };
 };
 
@@ -29,11 +54,11 @@ const deleteRestaurant = async (req,res) => {
   };
 };
 const deleteEvent = async (req, res) => {
-try {
-  
-} catch (error) {
-  
-};
+  try {
+
+  } catch (error) {
+
+  };
 };
 
 ////////////////////////////////////////////////////////////////////////////////
